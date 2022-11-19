c1daf64efd899fd1a407b8921d518ae609348f05
Matthew Bays <matthew.bays@ontariosystems.com>
2022-11-19 14:08:03 -0500

    Use github for the release process

--- update.js
+++ update.js
@@ -3,8 +3,7 @@ const fs = require('fs');
 var packagejson = require('./package.json');
 
 packagejson.build.publish = [{
-    url: "https://github.com/ontariosystems/OpenLens/releases/download/Latest",
-    provider: "generic"
+    provider: "github"
 }];
 
 // This updates the naming of the artifact to not contain spaces vs the default 'OpenLens Setup ${version}.${ext}'
