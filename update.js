80ce2e5e3bee11ffed1892e96bc14f92bd8090d8
Matthew Bays <matthew.bays@ontariosystems.com>
2022-11-30 13:40:33 -0500

    Add code signing and update windows naming

--- update.js
+++ update.js
@@ -8,6 +8,6 @@ packagejson.build.publish = [{
 }];
 
 // This updates the naming of the artifact to not contain spaces vs the default 'OpenLens Setup ${version}.${ext}'
-packagejson.build.win.artifactName = "OpenLens.Setup.${version}.${ext}";
+packagejson.build.win.artifactName = "OpenLens-${version}.${ext}";
 
 fs.writeFileSync('package.json', JSON.stringify(packagejson));
